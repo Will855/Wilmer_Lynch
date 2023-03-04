3957b6f331db8545f2456059d973d870b6f8a6ca
Wilmer <73180437+Will855@users.noreply.github.com>
2023-03-04 14:37:33 -0400

    Add files via upload

---
+++ Portfolio web/JS/App.js
@@ -0,0 +1,10 @@
+let span_1 = document.querySelector(".line1__bars-menu");
+let span_2 = document.querySelector(".line2__bars-menu");
+let span_3 = document.querySelector(".line3__bars-menu");
+let content_nav = document.querySelector(".content-nav");
+let list = document.querySelector(".list");
+let menuToggle = document.querySelector(".menuToggle");
+
+menuToggle.onclick = () => {
+  menuToggle.classList.toggle("active");
+};
